c2879dcd2158c4ca3fa491715da6797cdc53e8dd
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2024-11-15 21:53:41 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -24,7 +24,7 @@ async def ping(_, message):
     rm = await message.reply_text("Pinging....", quote=True)
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"Ping 🔥!\n{time_taken_s:.3f} ms")
+    await rm.edit(f"Ping 🔥\n{time_taken_s:.3f} ms")
     return time_taken_s
 
 
